1aba69c6fd98bd346131ef8bc7fe8ae5686993c0
chirantan2003 <chirantan2003@gmail.com>
2022-08-18 14:15:14 +0530

    style: linting css

--- components/card.js
+++ components/card.js
@@ -1,20 +1,20 @@
-import { useRouter } from 'next/router'
-import styles from './card.module.css'
-import Image from 'next/image'
+import { useRouter } from "next/router";
+import styles from "./card.module.css";
+import Image from "next/image";
 
-export default function Cards (props) {
-  const router = useRouter()
+export default function Cards(props) {
+  const router = useRouter();
 
-  function checkVal () {
+  function checkVal() {
     router.push(
       {
-        pathname: '../formJs',
+        pathname: "../formJs",
         query: {
-          title: props.title
-        }
+          title: props.title,
+        },
       },
-      'form'
-    )
+      "form"
+    );
   }
 
   return (
@@ -26,11 +26,11 @@ export default function Cards (props) {
         <div className={styles.border}>
           <h2>{props.title}</h2>
           <p>{props.content}</p>
-          <button className={styles.button} onClick={checkVal} type='submit'>
-            Send
+          <button className={styles.button} onClick={checkVal} type="submit">
+            Search
           </button>
         </div>
       </div>
     </>
-  )
+  );
 }
