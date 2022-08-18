3fd80c61ba566a38d9c4638b7f448357dbed1e6e
chirantan2003 <chirantan2003@gmail.com>
2022-08-18 21:05:10 +0530

    fix: footer position

--- pages/_app.js
+++ pages/_app.js
@@ -23,8 +23,8 @@ export default function App ({ Component, pageProps }) {
       <Layout />
       <div className='body'>
         <Component {...pageProps} />
-        <Footer />
       </div>
+      <Footer />
     </>
   )
 }
